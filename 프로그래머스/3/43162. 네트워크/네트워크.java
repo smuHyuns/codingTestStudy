156262da2851c381be37842f239096f739c2bc68
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-07-18 09:35:10 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,42 +1,25 @@
 class Solution {
     
     boolean[] visited;
-    int [][] graph;
     public int solution(int n, int[][] computers) {
-        visited = new boolean[n]; //방문여부
-        graph = new int[n][n]; //관계연결
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(computers[i][j] == 1){
-                    graph[i][j] = 1;
-                    graph[j][i] = 1;
-                }
-            }
-        }
-        
-        int answer = 0;
-        
+        visited = new boolean[n]; //방문여부     
+        int answer = 0; 
+    
         for(int i=0; i<n; i++){
             if(!visited[i]){
                 answer++;
-                dfs(i);
+                dfs(i, computers);
             }
         }
-        
         
         return answer;
     }
     
-    public void dfs(int start){
-        int n = graph.length;
-        if(!visited[start]){
-            visited[start] = true;
-        }
-        
-        for(int i=0; i<n; i++){
-            if(graph[start][i] ==1 && !visited[i])
-                dfs(i);
+    public void dfs(int start, int[][] computers){
+        visited[start] = true;
+        for(int i=0; i<computers.length; i++){
+            if(!visited[i] && computers[start][start] == computers[start][i])
+                dfs(i,computers);
         }
     }
 }
