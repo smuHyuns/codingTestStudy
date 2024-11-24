6b7ee980f33fce41bd80918609d4773bf2a2d5a5
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-11-24 17:10:15 +0900

    [Bronze V] Title: A/B, Time: 192 ms, Memory: 17936 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b= sc.nextDouble();
+        System.out.println(a/b);
+    }
+}
