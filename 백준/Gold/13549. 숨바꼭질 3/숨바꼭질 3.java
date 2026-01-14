3b5027f6cf1feb1399908b95d7ed93f9f2d4e351
최현수 <106013493+smuHyuns@users.noreply.github.com>
2026-01-14 20:23:16 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 200 ms, Memory: 19404 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int start = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+
+        // 걷기 - 1초뒤 -1 / +1
+        // 순간이동 - 0초뒤 2*현재위치
+
+        int[] times = new int[100001];
+
+        Arrays.fill(times, Integer.MAX_VALUE);
+        times[start] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        pq.offer(new int[]{start, 0});
+
+        while (!pq.isEmpty()) {
+            int[] now = pq.poll();
+            int no = now[0];
+            int time = now[1];
+
+            if (isValid(no * 2)) {
+                if (times[no * 2] > time) {
+                    times[no * 2] = time;
+                    pq.offer(new int[]{no * 2, time});
+                }
+            }
+            if (isValid(no + 1)) {
+                if (times[no + 1] > time + 1) {
+                    times[no + 1] = time + 1;
+                    pq.offer(new int[]{no + 1, time + 1});
+                }
+            }
+            if (isValid(no - 1)) {
+                if (times[no - 1] > time + 1) {
+                    times[no - 1] = time + 1;
+                    pq.offer(new int[]{no - 1, time + 1});
+                }
+            }
+        }
+
+        System.out.println(times[target]);
+    }
+
+
+    public static boolean isValid(int num) {
+        return num >= 0 && num <= 100000;
+    }
+}
